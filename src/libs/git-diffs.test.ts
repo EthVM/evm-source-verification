7d432794520e563fa0350712cfeba04277300ca6
nickkelly1 <nick.kelly314+github@gmail.com>
2022-02-08 17:05:16 -0800

    origin: wip

--- src/libs/git-diffs.test.ts
+++ src/libs/git-diffs.test.ts
@@ -5,18 +5,19 @@ describe('diffs', () => {
   it('todo: should work', async () => {
     // Create GitHub client with the API token.
     const token = process.env.GITHUB_TOKEN!;
-    const glient = getOctokit(token);
+    const client = getOctokit(token);
 
     // https://api.github.com/repos/octocat/linguist/compare/master...octo-org:master
     try {
       // https://api.github.com/repos/linguist/octocat/compare/master...octo-org%3Amaster
       // https://api.github.com/repos/octocat/linguist/compare/master...octo-org:master
       // const response = await client.rest.repos.compareCommits({
-      //   base: 'devop/pr-gh-action',
-      //   head: 'test/gha',
+      //   base: 'EthVM:devop/gha',
+      //   head: 'nickkelly1:devop/gha',
       //   owner: 'EthVM',
       //   repo: 'evm-source-verification',
       // })
+      // console.log(response);
     } catch (err) {
       console.error('ERROR:', err);
       throw err;
